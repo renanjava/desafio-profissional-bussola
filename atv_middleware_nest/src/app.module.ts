2edb52913b0da834b4bfdc414fb035fd31ee60db
Renan Geraldini Leão <131619149+renanjava@users.noreply.github.com>
2024-05-30 23:49:13 -0300

    Add files via upload

--- atv_middleware_nest/src/app.module.ts
+++ atv_middleware_nest/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot("mongodb://127.0.0.1:27017/nest"),
+            ProductsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
