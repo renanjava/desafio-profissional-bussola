e1a0bd259d97f6a0d26aec13cd6fb71287d5c3bc
Renan Geraldini Leão <131619149+renanjava@users.noreply.github.com>
2024-06-23 23:53:16 -0300

    fix

--- trabalho_jwt/trabalho-jwt/src/exception/all-exceptions.filter.ts
+++ trabalho_jwt/trabalho-jwt/src/exception/all-exceptions.filter.ts
@@ -1,20 +1,13 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
-@Catch()
-export class AllExceptionsFilter implements ExceptionFilter {
-  catch(exception: unknown, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+export class CustomException extends HttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
 
-    const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
-
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      message: exception instanceof Error ? exception.message : exception,
-    });
+export class ValidationException extends HttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.BAD_REQUEST);
   }
 }
