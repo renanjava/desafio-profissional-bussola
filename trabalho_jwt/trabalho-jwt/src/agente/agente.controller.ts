223a4186c28871beb69d726608e004a7e809e534
AlissonKobayashi <134564328+AlissonKobayashi@users.noreply.github.com>
2024-06-23 23:44:01 -0300

    Update agente.controller.ts

--- trabalho_jwt/trabalho-jwt/src/agente/agente.controller.ts
+++ trabalho_jwt/trabalho-jwt/src/agente/agente.controller.ts
@@ -3,6 +3,8 @@ import { AgenteService } from './agente.service';
 import { CreateAgenteDto } from './dto/create-agente.dto';
 import { UpdateAgenteDto } from './dto/update-agente.dto';
 import { AuthGuard } from '../auth/auth.guard';
+import { CustomException, ValidationException } from '../exception/exception'; 
+
 
 @Controller('agente')
 export class AgenteController {
@@ -11,36 +13,60 @@ export class AgenteController {
   @UseGuards(AuthGuard)
   @Post('/consumir-api')
   createFromValorantApi(){
-    return this.agenteService.createFromValorantApi();
+    try{
+      return this.agenteService.createFromValorantApi();
+    } catch (error) {
+      throw new CustomException('Erro ao processar');
+    }
   }
 
   @UseGuards(AuthGuard)
   @Post()
   create(@Body() createAgenteDto: CreateAgenteDto) {
-    return this.agenteService.create(createAgenteDto);
+    try{
+      return this.agenteService.create(createAgenteDto);
+    } catch (error) {
+      throw new CustomException('Erro ao processar');
+    }
   }
 
   @UseGuards(AuthGuard)
   @Get()
   findAll() {
-    return this.agenteService.findAll();
+    try{
+      return this.agenteService.findAll();
+    } catch (error) {
+      throw new CustomException('Erro ao processar');
+    }
   }
 
   @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.agenteService.findOne(id);
+    try{
+      return this.agenteService.findOne(id);
+    } catch (error) {
+      throw new CustomException('Erro ao processar');
+    }
   }
 
   @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAgenteDto: UpdateAgenteDto) {
-    return this.agenteService.update(id, updateAgenteDto);
+    try{
+     return this.agenteService.update(id, updateAgenteDto);
+    } catch (error) {
+      throw new CustomException('Erro ao processar');
+    }
   }
 
   @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string): Promise<any> {
-    return this.agenteService.remove(id);
+    try{
+      return this.agenteService.remove(id);
+    } catch (error) {
+      throw new CustomException('Erro ao processar');
+    }
   }
 }
